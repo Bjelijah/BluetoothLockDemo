80d073e8f4ce95ec0da72a314726fa02fe32a678
elijah <elijah@live.cn>
2021-07-13 09:33:57 +0800

    接口

--- app/src/main/java/xyz/mercs/bluetoothlockdemo/MainActivity.kt
+++ app/src/main/java/xyz/mercs/bluetoothlockdemo/MainActivity.kt
@@ -14,11 +14,15 @@ import android.widget.Button
 import com.cbj.sdk.libbase.utils.LOG
 import com.cbj.sdk.libui.mvp.BaseActivity
 import com.cbj.sdk.libui.mvp.inflate
+import com.inuker.bluetooth.library.BluetoothClient
 import com.tbruyelle.rxpermissions2.RxPermissions
 import xyz.mercs.bluetoothlockdemo.databinding.ActivityMainBinding
 import xyz.mercs.bluetoothlockdemo.service.BlueService
+import xyz.mercs.bluetoothlockdemo.util.Util
 
 class MainActivity : BaseActivity() {
+
+    private val mTestMac = "CB:57:B3:DA:91:FD"
 
     private val mBinding: ActivityMainBinding by inflate()
 
@@ -28,6 +32,10 @@ class MainActivity : BaseActivity() {
     override fun getView(): View = mBinding.root
 
     override fun initView() {
+        BLEManager.sInstance.init(this,mTestMac){
+            LOG.I("123","we read ${Util.byte2HexStr(it)}")
+            parseMsg(it)
+        }
         mBinding.btn1.setOnClickListener {
             RxPermissions(this).request(
                 Manifest.permission.BLUETOOTH,
@@ -36,15 +44,58 @@ class MainActivity : BaseActivity() {
                 Manifest.permission.ACCESS_FINE_LOCATION
             ).subscribe {
                 LOG.I("123","openBlue")
-                openBlu()
+//                openBlu()
+                BLEManager.sInstance.connect()
             }
         }
         mBinding.btn2.setOnClickListener {
-            mBlueService?.connect()
+//            mBlueService?.connect()
+            var tokenReq = Util.sendGetTokenProtocol()
+
+            LOG.I("123","tokenReq=${Util.byte2HexStr(tokenReq)}")
+            BLEManager.sInstance.write(tokenReq)
+           // BLEManager.sInstance.read()
+        }
+        mBinding.btn3.setOnClickListener {
+            BLEManager.sInstance.write(Util.getPower(token))
         }
 
+    }
+
+
+    var token = ByteArray(4)
+
+    fun parseMsg(bs:ByteArray){
+        if ((bs[0] == 0x06.toByte()) && (bs[1] == 0x02.toByte())){
+            LOG.I("123","token = ${String.format("0x%x",bs[3])} ${String.format("0x%x",bs[4])}" +
+                    " ${String.format("0x%x",bs[5])} ${String.format("0x%x",bs[6])}")
+            token[0] = bs[3]
+            token[1] = bs[4]
+            token[2] = bs[5]
+            token[3] = bs[6]
+
+//            token[0] = bs[6]
+//            token[1] = bs[5]
+//            token[2] = bs[4]
+//            token[3] = bs[3]
+
+        }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
